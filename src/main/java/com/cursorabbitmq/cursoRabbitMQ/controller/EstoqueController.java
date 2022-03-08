fa3f3f966f5f86d02a5bfd620f9f798dc313f1b2
“StellaCorreiaF” <stellafernanda@icloud.com>
2022-03-08 12:11:46 -0300

    cria consumidor

--- src/main/java/com/cursorabbitmq/cursoRabbitMQ/controller/EstoqueController.java
+++ src/main/java/com/cursorabbitmq/cursoRabbitMQ/controller/EstoqueController.java
@@ -1,22 +1,38 @@
 package com.cursorabbitmq.cursoRabbitMQ.controller;
 
+import com.cursorabbitmq.cursoRabbitMQ.constantes.RabbitMQConstantes;
 import com.cursorabbitmq.cursoRabbitMQ.dto.EstoqueDto;
+import com.cursorabbitmq.cursoRabbitMQ.service.RabbitMQService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Component
 @RestController
 @RequestMapping(value="estoque")
 public class EstoqueController {
+
+    @Autowired
+    private RabbitMQService rabbitMQService;
     @PutMapping
     private ResponseEntity alteraEstoque(@RequestBody EstoqueDto estoqueDto){
+        System.out.println(estoqueDto.codigoProduto);
+
+        this.rabbitMQService.enviarMensagem(RabbitMQConstantes.FILA_ESTOQUE, estoqueDto);
 
         return new ResponseEntity(HttpStatus.OK);
-
-
+    }
+    @RabbitListener(queues = RabbitMQConstantes.FILA_ESTOQUE)
+    private void consumidor(EstoqueDto estoqueDto){
+        System.out.println(estoqueDto.codigoProduto);
+        System.out.println(estoqueDto.quantidade);
+        System.out.println("------------------------");
 
     }
+
 }
--- src/main/java/com/cursorabbitmq/cursoRabbitMQ/controller/PrecoController.java
+++ src/main/java/com/cursorabbitmq/cursoRabbitMQ/controller/PrecoController.java
@@ -1,6 +1,9 @@
 package com.cursorabbitmq.cursoRabbitMQ.controller;
 
+import com.cursorabbitmq.cursoRabbitMQ.constantes.RabbitMQConstantes;
 import com.cursorabbitmq.cursoRabbitMQ.dto.PrecoDto;
+import com.cursorabbitmq.cursoRabbitMQ.service.RabbitMQService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,9 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value="preco")
 public class PrecoController {
+    @Autowired
+    private RabbitMQService rabbitMQService;
 
     @PutMapping
-    private ResponseEntity alterarPReco(@RequestBody PrecoDto precoDto) {
+    private ResponseEntity alterarPreco(@RequestBody PrecoDto precoDto) {
+        System.out.println(precoDto.preco);
+
+        this.rabbitMQService.enviarMensagem(RabbitMQConstantes.FILA_PRECO, precoDto);
         return new ResponseEntity(HttpStatus.OK);
     }
 }
